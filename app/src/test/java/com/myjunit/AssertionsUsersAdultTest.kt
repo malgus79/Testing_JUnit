a56d80bfe0fce79e9ea6571c65518bf11f8997a9
Gustavo Maldonado <gamcba7982@gmail.com>
2022-11-12 12:31:21 -0300

    Rule solution to inheritance

--- app/src/test/java/com/myjunit/AssertionsUsersAdultTest.kt
+++ app/src/test/java/com/myjunit/AssertionsUsersAdultTest.kt
@@ -1,15 +1,18 @@
 package com.myjunit
 
 import org.junit.After
-import org.junit.Assert.assertFalse
-import org.junit.Assert.assertTrue
+import org.junit.Assert.*
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 
 internal class AssertionsUsersAdultTest {
 
     private lateinit var bot: User
     private lateinit var juan: User
+
+    @get:Rule
+    val locationESRule = LocationESRule()
 
     @Before
     fun setup(){
@@ -25,11 +28,11 @@ internal class AssertionsUsersAdultTest {
 
     @Test
     fun isAdult() {
-        val assertions = Assertions()
+        /*val assertions = Assertions()
         assertions.setLocation("ES")
         assertTrue(assertions.isAdult(juan))
-        assertTrue(assertions.isAdult(bot))
-//        assertEquals(true, locationESRule.assertions?.isAdult(juan))
-//        assertEquals(true, locationESRule.assertions?.isAdult(bot))
+        assertTrue(assertions.isAdult(bot))*/
+        assertEquals(true, locationESRule.assertions?.isAdult(juan))
+        assertEquals(true, locationESRule.assertions?.isAdult(bot))
     }
 }
